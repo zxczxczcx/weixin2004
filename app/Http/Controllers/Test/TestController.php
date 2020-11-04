d1848426eaf95fe05b8fc60d5b47245f71c9b455
wangningyu <2281685302@qq.com>
2020-11-04 20:12:07 +0800

    测试项目

---
+++ app/Http/Controllers/Test/TestController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Test;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redis;
+
+class TestController extends Controller
+{
+    public function test(){
+
+        // echo PHPinfo();die;
+        //连接数据库
+        $a = DB::table('p_users')->limit(5)->get();
+        
+        //redis
+        $key = 'user';
+        Redis::set($key,time());
+        $time = Redis::get($key);
+        echo strtotime(date('Y-m-d'),$time);
+    }
+}
