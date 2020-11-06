dea9d8829f015e8d55f254ade22eae484e27ad94
wangningyu <2281685302@qq.com>
2020-11-06 15:50:42 +0800

    小测试

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class ApiController extends Controller
 {
+    /**
+     * Undocumented function
+     *         微信接口调用
+     * @return void
+     */
     public function checkwx(){
         $signature = $_GET["signature"];
         $timestamp = $_GET["timestamp"];
@@ -18,9 +24,38 @@ class ApiController extends Controller
         $tmpStr = sha1( $tmpStr );
         
         if( $tmpStr == $signature ){
-            echo $_GET['echostr'];
+            // echo $_GET['echostr'];
+            
+            //接受数据
+            $xml_str = file_get_contents('php://input');
+            //写入文档
+            file_put_contents('wx_logs.log',$xml_str,FILE_APPEND);
+            
         }else{
             echo 111;
         }
     }
+
+    /**
+     * access_token
+     */
+    public function Aoken(){
+        $key = 'wx:access_token';
+        $token = Redis::get($key);
+        if($token){
+            echo $key;
+        }else{
+            $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".env('WX_APPID')."&secret=".env('WX_APPSECRET');
+        
+        $ken = file_get_contents($url);
+        $data = json_decode($ken,true);
+        
+        Redis::set($key,$data['access_token']);
+        Redis::expire($key,3600);
+        
+
+        }
+        
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('/', function () {
 Route::get('/test','Test\TestController@test');//测试
 
 Route::get('/wx','ApiController@checkwx');//测试接口
+Route::get('/ken','ApiController@Aoken');//access_token
