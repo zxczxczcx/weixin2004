6778dafef7a35f260403078082db2c4f3014acd5
wangningyu <2281685302@qq.com>
2020-11-09 18:52:30 +0800

    测试

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -26,11 +26,23 @@ class ApiController extends Controller
         $tmpStr = sha1( $tmpStr );
         
         if( $tmpStr == $signature ){
-            $data = file_get_contents("php://input");
-            $data_res = simplexml_load_string($data,"SimplXMLement",LIBXML_NOCDATA);
-            file_put_contents('wx.error.log',$data_res);
-            if($data_res['MsgType']=='event'){  // 事件
-                
+            //获取xml 格式
+            $postArr = file_get_contents("php://input");
+            $post_obj = simplexml_load_string($postArr,"SimplXMLement",LIBXML_NOCDATA);
+            file_put_contents('wx_error.logs',$post_obj,FILE_APPEND);
+            if($post_obj->MsgType=='Event'){  // 事件
+                //关注
+                if($post_obj->Event == 'subscribe'){
+
+                    $toUser = $post_obj->ToUserName;
+                    $fromUser   = $post_obj->FromUserName;
+                    //回复用户文本  信息  格式
+                    $msgText = 'text';
+                    $content = '欢迎关注';
+                    $this->WxText($toUser,$fromUser,$content);
+
+
+                }
             }
         }else{
             echo $_GET['echostr'];
@@ -53,13 +65,24 @@ class ApiController extends Controller
         echo Redis::get($key);
     }
 
-    /**发送消息 */
-    public function ResponseTest(){
-        
-
+    /**
+     * 回复文本  WxText
+     */
+    public function WxText($toUser,$fromUser,$content){
+        $template = "<xml>
+                    <ToUserName><![CDATA[%s]]></ToUserName>
+                    <FromUserName><![CDATA[%s]]></FromUserName>
+                    <CreateTime>%s</CreateTime>
+                    <MsgType><![CDATA[%s]]></MsgType>
+                    <Content><![CDATA[%s]]></Content>
+                    </xml>";
+        $info = sprintf($template, $toUser, $fromUser, time(), 'text', $content);
+        return $info;
     }
 
-   
+
+    
+
 
 
 
